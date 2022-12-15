444d6075724dfb76524930d8d3a444cc46a65e09
sanjeev <sanjeevsingh.77d@gmail.com>
2022-12-15 21:19:38 +0530

    Create product review management

---
+++ ProdductReviewManagement/ProdductReviewManagement/Management.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdductReviewManagement
+{
+    public class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+
+        public void TopRecords(List<ProductReview> review)
+        {
+            var recordeData = (from productReviews in review
+                               orderby productReviews.Rating descending
+                               select productReviews).Take(3);
+
+            Console.WriteLine($"topReview : {JsonConvert.SerializeObject(recordeData)}");
+        }
+         
+}
+}
---
+++ ProdductReviewManagement/ProdductReviewManagement/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ProdductReviewManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t ================= WELCOME TO PRODUCT REVIEW MANAGEMENT PROBLEM =======================\n");
+
+            List<ProductReview> productReviews = new List<ProductReview>()
+            {
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 2, UserID = 1, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 3, UserID = 2, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 4, UserID = 2, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 5, UserID = 3, Rating = 2, Review = "nice", isLike = false },
+                new ProductReview() { ProductID = 6, UserID = 4, Rating = 1, Review = "Bad", isLike = false },
+                new ProductReview() { ProductID = 7, UserID = 3, Rating = 1.5, Review = "nice", isLike = true },
+                new ProductReview() { ProductID = 8, UserID = 8, Rating = 4, Review = "nice", isLike = false },
+                new ProductReview() { ProductID = 9, UserID = 9, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 10, UserID = 10, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 11, UserID = 11, Rating = 4, Review = "nice", isLike = true },
+                new ProductReview() { ProductID = 12, UserID = 12, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 13, UserID = 13, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 14, UserID = 14, Rating = 5, Review = "Good", isLike = true },
+
+               
+            };
+
+
+
+            //foreach (var list in productReviews)
+            //{
+            //    Console.WriteLine($"ProductID : {list.ProductID}\n" +
+            //                      $"UserID    : {list.UserID}\n" +
+            //                      $"Rating    : {list.Rating}\n" +
+            //                      $"Review    : {list.Review}\n" +
+            //                      $"isLike    : {list.isLike}\n");
+            //}
+
+            foreach (ProductReview product in productReviews)
+            {
+                Console.WriteLine("ProductID : " + product.ProductID + " UserID : " + product.UserID + " Rating : " + product.Rating + " Review : " + product.Review + " IsLike : " + product.isLike + "\n");
+            }
+
+            // Create object of Management class
+            Management objManage = new Management();
+            objManage.TopRecords(productReviews);
+           
+        }
+    }
+}
